2dc70f37cd3239aadc7e698006c63b7692a7e4b3
gaiagent <gaiagent0@gmail.com>
2026-04-03 19:44:03 +0200

    fix: pass onMenuClick + onProfileClick to all screens

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ export default function App() {
 
   const renderScreen = () => {
     switch (activeTab) {
-      case 'bingo':     return <BingoScreen user={user} onNavigate={setActiveTab} />
-      case 'community': return <CommunityScreen user={user} onNavigate={setActiveTab} />
-      case 'news':      return <NewsScreen onNavigate={setActiveTab} />
-      case 'settings':  return <SettingsScreen user={user} loading={loading} onNavigate={setActiveTab} />
-      default:          return <BingoScreen user={user} onNavigate={setActiveTab} />
+      case 'bingo':     return <BingoScreen user={user} onNavigate={setActiveTab} onMenuClick={goToSettings} onProfileClick={goToSettings} />
+      case 'community': return <CommunityScreen user={user} onNavigate={setActiveTab} onMenuClick={goToSettings} onProfileClick={goToSettings} />
+      case 'news':      return <NewsScreen onNavigate={setActiveTab} onMenuClick={goToSettings} onProfileClick={goToSettings} />
+      case 'settings':  return <SettingsScreen user={user} loading={loading} onNavigate={setActiveTab} onMenuClick={goToSettings} onProfileClick={goToSettings} />
+      default:          return <BingoScreen user={user} onNavigate={setActiveTab} onMenuClick={goToSettings} onProfileClick={goToSettings} />
     }
   }
 
   return (
     <div className="relative min-h-screen bg-background font-body">
-      {renderScreen({ goToSettings })}
+      {renderScreen()}
       <BottomNav active={activeTab} onNavigate={setActiveTab} />
     </div>
   )
